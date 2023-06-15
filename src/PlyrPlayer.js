e444986359d0923774273b0f3d82d3db5ceaa970
arthgupta2003 <62980360+arthgupta2003@users.noreply.github.com>
2023-06-15 13:25:17 +0530

    fixed plyr styling

--- src/PlyrPlayer.js
+++ src/PlyrPlayer.js
@@ -1,11 +1,21 @@
 import Plyr from 'plyr-react';
 import React, { Component } from 'react';
 import Hls from 'hls.js';
+import "plyr-react/plyr.css"
+
+const videoOptions = {
+  seekTime: 5,
+  speed: {
+    selected: 1,
+    options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.5, 4]
+  }
+};
 
 export default class PlyrPlayer extends Component {
   constructor(props) {
     super(props);
     this.ref = React.createRef();
+    this.src= props.src
   }
 
   componentDidMount() {
@@ -15,7 +25,7 @@ export default class PlyrPlayer extends Component {
   loadVideo = async () => {
     const video = document.getElementById('plyr');
     const hls = new Hls();
-    hls.loadSource('https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8');
+    hls.loadSource(this.src);
     hls.attachMedia(video);
 
     this.ref.current.plyr.media = video;
@@ -29,9 +39,10 @@ export default class PlyrPlayer extends Component {
     return (
       <Plyr
         id="plyr"
-        options={{ volume: 0.1 }}
+        options={videoOptions}
         source={{}}
         ref={this.ref}
+        controls={false}
       />
     );
   }
